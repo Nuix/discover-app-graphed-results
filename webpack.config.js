4ee40e6f2c576e757a9e03be30149176be310ce9
Collin Arnold <collin@collinarnold.net>
2018-03-17 13:05:13 -0700

    starting app setup and bundling css via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,11 @@ module.exports = {
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }]
     }
 };
