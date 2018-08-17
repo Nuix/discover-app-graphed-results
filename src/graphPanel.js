12b47e0cf9d752e2adeddc90582aee39b5b89562
Collin Arnold <collin.arnold@fticonsulting.com>
2018-08-16 21:36:04 -0700

    minor cleanup

--- src/graphPanel.js
+++ src/graphPanel.js
@@ -7,8 +7,8 @@ function GraphPanel(parentEl, state) {
 
     //'#graph'
     this.parentEl = document.querySelector(parentEl);       // The container element to render the graph into
-    this.graphData = null;          // Populated when results change via Ringtail API query
-    this.chart = null;              // dcjs rendered chart
+    this.graphData = null;                                  // Populated when results change via Ringtail API query
+    this.chart = null;                                      // dcjs rendered chart
 
     this.activeField = state.activeField || null;           // Field the user selected to graph coding for
     this.activeGraphType = state.activeGraphType || 'bar';  // Type of graph to draw
@@ -28,11 +28,16 @@ function GraphPanel(parentEl, state) {
                 { id: 'x', name: 'X-Axis Counts' },
             ], 'axis') + '\
         </div>\
+        <div class="graph"></div>\
     </div>';
 }
 
+function newId(prefix) {
+    return prefix + '-' + Math.random().toString(36).substring(2);
+}
+
 function buildCombo(value, choices, cls, placeholder) {
-    var listId = 'list-' + Math.random().toString(36).substring(2);
+    var listId = newId('list');
     return '\
     <input class="select ' + cls + '" type="text" list="' + listId + '" value="' + value + '" placeholder="' + (placeholder || '') + '" />\
     <datalist id="' + listId + '">\
