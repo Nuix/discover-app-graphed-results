8d0d71e6a483fcc62d1e924b866c858511ab64c4
Collin Arnold <collin.arnold@nuix.com>
2018-11-21 14:41:12 -0800

    replacing the manual dev server with webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,19 @@ const webpack = require('webpack');
 module.exports = {
     mode: 'development',
     entry: ['whatwg-fetch', './src/index.js'],
+    devServer: {
+        index: 'index.html',
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: process.env.GRAPHED_RESULTS_PORT || 12345
+    },
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
-            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }, 
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
             { test: /\.(jpe?g|png|gif)$/i, loader: "file-loader" },
         ]
     },
